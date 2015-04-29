402ed5d814ccd148d2fc617cff4b5f0d7f2a74f7
Jennifer Bolanos & Jessica Iler <student@epicodus.com>
2015-04-29 14:25:41 -0700

    return a vehicle by its id number

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -45,4 +45,14 @@ class Vehicle
   define_method(:id) do
     @id
   end
+
+  define_singleton_method(:find) do |identification|
+    found_vehicle = nil
+    @@vehicles.each() do |vehicle|
+      if vehicle.id().eql?(identification.to_i())
+        found_vehicle = vehicle
+      end
+    end
+    found_vehicle
+  end  
 end
