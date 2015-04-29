80f0c43cda722f5b6cf14e7f6cd93375d63ffd5b
Jennifer Bolanos & Jessica Iler <student@epicodus.com>
2015-04-29 11:44:00 -0700

    it will return the make, model, and year of a vehicle

---
+++ lib/vehicle.rb
@@ -0,0 +1,22 @@
+class Vehicle
+  @@vehicles = []
+
+  define_method(:initialize) do |make, model, year|
+    @make = make
+    @model = model
+    @year = year
+  end
+
+  define_method(:make) do
+    @make
+  end
+
+  define_method(:model) do
+    @model
+  end
+
+  define_method(:year) do
+    @year
+  end
+
+end
---
+++ spec/vehicle_spec.rb
@@ -0,0 +1,26 @@
+require('rspec')
+require('vehicle')
+
+describe(Vehicle) do
+  describe("#make") do
+    it("returns the make of the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      expect(test_vehicle.make()).to(eq("Toyota"))
+    end
+  end
+
+  describe("#model") do
+    it("returns the model fo the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      expect(test_vehicle.model()).to(eq("Prius"))
+    end
+  end
+  describe("#year") do
+    it("returns the year of the vehicle") do
+      test_vehicle = Vehicle.new("Toyota", "Prius", 2000)
+      #test_vehicle.save()
+      expect(test_vehicle.year()).to(eq(2000))
+    end
+  end
+  
+end
